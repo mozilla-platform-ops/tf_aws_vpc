2225d43fbdf9ed0c329fb30bf56efaacb1458ebf
Jake Watkins <jwatkins@mozilla.com>
2016-05-18 16:03:54 -0700

    Add empty defaults to azs vars

--- variables.tf
+++ variables.tf
@@ -2,8 +2,8 @@ variable "name" { }
 variable "cidr" { }
 variable "public_subnets" { default = "" }
 variable "private_subnets" { default = "" }
-variable "azs_private" { }
-variable "azs_public" { }
+variable "azs_private" { default = "" }
+variable "azs_public" { default = "" }
 variable "enable_dns_hostnames" {
   description = "should be true if you want to use private DNS within the VPC"
   default = false

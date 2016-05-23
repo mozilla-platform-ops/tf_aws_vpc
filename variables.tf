b40598ac70393a0f05dae2aee03e4b7469b10d2e
Jake Watkins <jwatkins@mozilla.com>
2016-05-23 11:28:09 -0700

    Clean up variables and set enable_dns_support to match provider default

--- variables.tf
+++ variables.tf
@@ -1,14 +1,37 @@
-variable "name" { }
-variable "cidr" { }
-variable "public_subnets" { default = "" }
-variable "private_subnets" { default = "" }
-variable "azs_private" { default = "" }
-variable "azs_public" { default = "" }
+variable "name" {
+    description = "The name of the vpc to be used in tagging"
+}
+
+variable "cidr" {
+    description = "IP CIDR block of the vpc network"
+}
+
+variable "public_subnets" {
+    description = "A comma delimited list of subnets in CIDR notation for public use"
+    default = ""
+}
+
+variable "private_subnets" {
+    description = "A comma delimted list of subnets in CIDR notation for private use"
+    default = ""
+}
+
+variable "azs_public" {
+    description = "A comma delimted list of Availablity Zone for each public subnet listed. Element count must match public_subnets"
+    default = ""
+}
+
+variable "azs_private" {
+    description = "A comma delimted list of Availablity Zone for each private subnet listed. Element count must match private_subnets"
+    default = ""
+}
+
 variable "enable_dns_hostnames" {
-  description = "should be true if you want to use private DNS within the VPC"
+  description = "True if you want to use private DNS within the VPC"
   default = false
 }
+
 variable "enable_dns_support" {
-  description = "should be true if you want to use private DNS within the VPC"
-  default = false
+  description = "True if you want to use private DNS within the VPC"
+  default = true
 }
